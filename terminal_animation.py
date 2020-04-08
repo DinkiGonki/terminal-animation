438fc0a742901b645eed621f9010643d076fc858
LencoDigitexer <vkontakte258@gmail.com>
2020-04-08 18:39:56 +0300

    update for python3

--- terminal_animation.py
+++ terminal_animation.py
@@ -9,22 +9,28 @@ Author: mdq3
 2012/05/16
 
 """
-
+from sys import platform
 import time
 import os
-import animation
+import devsoft
 
-duration = 0.05    # The duration of time in seconds between each frame.
+duration = 0.15    # The duration of time in seconds between each frame.
 cycles = 100       # The number of cycles the animation plays through.
+command = ""
+
+if platform == "linux" or platform == "linux2":
+    command = "clear"
+elif platform == "win32":
+    command = "cls"
 
 def animate():
     """Iterate through the frames, printing then clearing each one to create an animation."""
     count = 0
     while count < cycles:
-        for frame in animation.frames:
-            print '\033[1;34m' + frame + '\033[1;m'  # Print the frame in color blue.
+        for frame in devsoft.frames:
+            print(frame)  # Print the frame in color blue.
             time.sleep(duration)
-            print(os.system('clear'))
+            print(os.system(command))
         count = count + 1
 
 animate()
